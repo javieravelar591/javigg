c5212727dd4f718c45f61479385a52386010da61
javieravelar591 <avela020@umn.edu>
2024-05-19 16:57:52 -0500

    fixed api calls, added form but form isn't working yet and messes up url

--- src/main/java/dev/javis/javigg/user/SummonerController.java
+++ src/main/java/dev/javis/javigg/user/SummonerController.java
@@ -2,19 +2,23 @@ package dev.javis.javigg.user;
 
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import ch.qos.logback.core.model.Model;
 import jakarta.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@RestController
+
+@Controller
 public class SummonerController {
 
     private static final Logger logger = LoggerFactory.getLogger(SummonerController.class);
@@ -30,7 +34,7 @@ public class SummonerController {
     
 
     private final RestTemplate restTemplate;
-
+    
     public SummonerController() {
         this.restTemplate = new RestTemplate();
     }
@@ -41,26 +45,37 @@ public class SummonerController {
         System.out.println("Account API URL: " + accountApiUrl);
         System.out.println("Summoner API URL: " + summonerApiUrl);
     }
-  
-    @GetMapping({"/", "/home"})
-    String home() {
-        return "Main Page";
+
+    // landing page
+    @RequestMapping({"/", "/home"})
+    public String home() {
+        return "index";
     }
 
     @GetMapping("/summoner/{gameName}/{tagLine}")
     public Summoner getSummoner(@PathVariable String gameName, @PathVariable String tagLine) {
-        String accountUrl = String.format("%s/riot/account/v1/accounts/by-riot-id/%s/%s?api_key=%s", 
+        try{
+            // get puuid
+            String accountUrl = String.format("%s/riot/account/v1/accounts/by-riot-id/%s/%s?api_key=%s", 
                                           accountApiUrl, gameName, tagLine, apiKey);
-        Account accountDto = restTemplate.getForObject(accountUrl, Account.class);
-        System.out.println(accountDto);
+            Account accountDto = restTemplate.getForObject(accountUrl, Account.class);
+            System.out.println(accountDto);
 
-        if (accountDto == null || accountDto.puuid() == null) {
-            throw new RuntimeException("Unable to fetch account details.");
+            if (accountDto == null || accountDto.puuid() == null) {
+                throw new RuntimeException("Unable to fetch account details.");
+            }
+
+            // get summoner info
+            String summonerUrl = String.format("%s/lol/summoner/v4/summoners/by-puuid/%s?api_key=%s",
+                                               summonerApiUrl, accountDto.puuid(), apiKey);
+            return restTemplate.getForObject(summonerUrl, Summoner.class);
+        } catch (HttpClientErrorException e) {
+            logger.error("HTTP error during API call: {}", e.getStatusCode(), e);
+            logger.error("Response body: {}", e.getResponseBodyAsString());
+            throw e;
+        } catch (Exception e) {
+            logger.error("Unexpected error during API call", e);
+            throw new RuntimeException("Unexpectred error during API call", e);
         }
-
-        // Step 2: Get Summoner information using PUUID
-        String summonerUrl = String.format("%s/lol/summoner/v4/summoners/by-puuid/%s?api_key=%s",
-                                           summonerApiUrl, accountDto.puuid(), apiKey);
-        return restTemplate.getForObject(summonerUrl, Summoner.class);
     }   
 }
