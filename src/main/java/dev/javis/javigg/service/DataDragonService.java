6f287eb4551091411e4acf3f55d7c142a547949a
Javier Avelar <javieravelar59@gmail.com>
2024-07-15 10:37:19 -0500

    had images working, decided I want to serve images from aws bucket instead, now not working

--- src/main/java/dev/javis/javigg/service/DataDragonService.java
+++ src/main/java/dev/javis/javigg/service/DataDragonService.java
@@ -20,10 +20,15 @@ public class DataDragonService {
     @Value("classpath:datadragon/data/en_US/profileicon.json")
     private Resource profileIconResource;
 
+    
+    @Value("${external.profileicon.url}")
+    private String externalProfileIconUrl;
+
     private final ObjectMapper objectMapper;
 
     private Map<String, Object> champData;
     private Map<String, Object> profileIconData;
+
 
     public DataDragonService(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
@@ -48,26 +53,27 @@ public class DataDragonService {
         this.profileIconData = objectMapper.readValue(profileIconResource.getInputStream(), Map.class);
     }
 
-    public String getProfileIconUrl(int profileIconId) {
-        System.out.println(profileIconData.get("data"));
-        System.out.println("iconId: " + profileIconId);
-        // logger.info("Fetching profile icon for ID: " + profileIconId);
-        Map<String, Object> data = (Map<String, Object>) profileIconData.get("data");
+    public String getProfileIconUrl(String profileIconId) {
+        // System.out.println(profileIconData.get("data"));
+        // System.out.println("iconId: " + profileIconId);
+        // // logger.info("Fetching profile icon for ID: " + profileIconId);
+        // Map<String, Object> data = (Map<String, Object>) profileIconData.get("data");
 
-        if (data != null) {
-            System.out.println(String.valueOf(profileIconId));
-            Map<String, Object> iconData = (Map<String, Object>) data.get(profileIconId);
-            if (iconData != null) {
-                Map<String, String> image = (Map<String, String>) iconData.get("image");
-                if (image != null) {
-                    String imageUrl = "/datadragon/images/profileicon/" + image.get("full");
-                    // logger.info("Profile icon URL: " + imageUrl);
-                    return imageUrl;
-                }
-            }
-        }
+        // if (data != null) {
+        //     Map<String, Object> iconData = (Map<String, Object>) data.get(profileIconId);
+        //     System.out.println(iconData);
+        //     if (iconData != null) {
+        //         Map<String, String> image = (Map<String, String>) iconData.get("image");
+        //         // System.out.println("image: " + image);
+        //         if (image != null) {
+        //             String imageUrl = "../../img/profileicon/" + image.get("full");
+        //             return imageUrl;
+        //         }
+        //     }
+        // }
         // logger.warning("Profile icon not found for ID: " + profileIconId);
-        return null;
+        return externalProfileIconUrl + profileIconId + ".png";
+        // return null;
     }
 
     public Map<String, Object> getProfileIconData() {
